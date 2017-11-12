3bcc6c511c0f7b732a3b29bc949829c9632f6e75
Allye <allye.vice@gmail.com>
2017-11-12 14:20:15 -0500

    we have a form!

--- client/scripts/components/newEvent.js
+++ client/scripts/components/newEvent.js
@@ -8,38 +8,48 @@ class NewEvent extends React.Component{
         this.handleChange = this.handleChange.bind(this);
         this.handleSave = this.handleSave.bind(this);
         this.state = {
-            m: Moment()
-        }
+            date: Moment(),
+            desc: '',
+            name: '',
+            loc: ''
+        };
+    }
+
+    handleInputChange(key, value) {
+        this.setState({
+        [key]: value,
+        });
     }
 
     handleChange(m){
         this.setState({
-        m: m,
+        date: m,
         });
     }
 
-    handleSave(){
-        console.log('saved', this.state.m);
+    handleSave(e){
+        e.preventDefault();
+        console.log('saved', this.state);
     }
 
     render(){
         return <form action="">
             <div>x</div>
-            <input type="text" placeholder="Event Title" name="title" id="title"/>
+            <input type="text" placeholder="Event Title" name="title" id="title" onChange={(e) => this.handleInputChange('name', e.target.value)}/>
             <div className="input">
-                <input type="text" value={this.state.m.format('llll')} readOnly />
+                <input type="text" value={this.state.date.format()} readOnly />
             </div>
             <InputMoment
-                moment={this.state.m}
+                moment={this.state.date}
                 onChange={this.handleChange}
-                onSave={this.handleSave}
                 minStep={1} // default
                 hourStep={1} // default
                 prevMonthIcon="ion-ios-arrow-left" // default
                 nextMonthIcon="ion-ios-arrow-right" // default
             />
-            <input type="text" name="loc" id="loc" placeholder="Location"/>
-            <textarea name="desc" id="desc" cols="30" rows="10" value="Description Here"></textarea>
+            <input type="text" name="loc" id="loc" placeholder="Location" onChange={(e) => this.handleInputChange('loc', e.target.value)}/>
+            <textarea name="desc" id="desc" cols="30" rows="10" placeholder="Description Here" onChange={(e) => this.handleInputChange('desc', e.target.value)}></textarea>
+            <button onClick={(e) => this.handleSave(e)}>Save</button>
         </form>
     }
 }
