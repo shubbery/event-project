41ba55f587338239c1688734bc9ec86f2df7a890
M.K. Reyes <mkgreyes@gmail.com>
2017-11-19 16:19:08 -0500

    create delete modal and delete links in event page

---
+++ client/scripts/components/deleteModal.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class DeleteModal extends React.Component{
+    constructor(){
+        super();
+    }
+    render(){
+        return <div className="delete-modal">
+            <p>Are you sure you want to delete this event?</p>
+            <a href="" className="cancel-delete" onClick={this.props.closeModal}>
+              No. Let's keep it!
+            </a>
+            <a href="" className="accept-delete">
+              Yes. #YOLO!
+            </a>
+          </div>;
+    }
+}
+
+export default DeleteModal;
--- client/scripts/components/eventPage.js
+++ client/scripts/components/eventPage.js
@@ -1,27 +1,62 @@
 import React from 'react';
+import DeleteModal from './deleteModal';
 
 class EventPage extends React.Component {
     constructor() {
         super();
+        this.editEvent = this.editEvent.bind(this);
+        this.deleteEvent = this.deleteEvent.bind(this);
+        this.closeModal = this.closeModal.bind(this);
         this.state = {
-            
+            deleteModal: false
         }
+    }
+    editEvent(e){
+        e.preventDefault();
+        console.log('editttttttttt');
+        console.log(this.props.match.params.eventId);
+    }
+    deleteEvent(e){
+        e.preventDefault();
+        console.log('deleteeeeeeeee');
+        console.log(this.props.match.params.eventId);
+        this.setState({
+            deleteModal: true
+        });
+    }
+    closeModal(e){
+        e.preventDefault();
+        console.log('close ze modal');
+        this.setState({
+            deleteModal: false
+        });
     }
     componentDidMount() {
         fetch(`/api/events/${this.props.match.params.eventId}`, {
             method: 'GET',
         })
         .then(res => res.json())
-        .then((event) => {
+        .then(event => {
             this.setState(event);
         });
     }
     render() {
-        return <div className='event'>
-            <pre>
-                {JSON.stringify(this.state, null, 3)}
-            </pre>
-        </div>
+        return <div className="event-page">
+            <ul className="admin-navigation">
+              <li>
+                <a href="" onClick={this.editEvent}>
+                  ✏️
+                </a>
+              </li>
+              <li>
+                <a href="" onClick={this.deleteEvent}>
+                  💀💀💀
+                </a>
+                {this.state.deleteModal ? (<DeleteModal closeModal={this.closeModal} deleteEvent={this.deleteEvent}/>) : null}
+              </li>
+            </ul>
+            <pre>{JSON.stringify(this.state, null, 3)}</pre>
+          </div>;
     }
 }
 
