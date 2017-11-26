b8368fbeb109043545c0c0922b07313aa9bcda72
Allye <allye.vice@gmail.com>
2017-11-26 17:41:56 -0500

    no difference at all really

--- api/Boards/model.js
+++ api/Boards/model.js
@@ -2,8 +2,4 @@ const mongoose = require('mongoose');
 
 const BoardSchema = new mongoose.Schema({
     name: String
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> 9a7f687cab48a40e6dfa21d0545a5d98d91cbd63
